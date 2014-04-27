4cd18a6f9fa48fb8bfaaef912ac492d21ada9465
teppeis <teppeis@gmail.com>
2014-04-28 01:22:07 +0900

    Ignore goog.i18n.CompactNumberFormatSymbols in goog/i18n/compactnumberformatsymbols_ext.js

--- closure/goog/i18n/compactnumberformatsymbols_ext.js
+++ closure/goog/i18n/compactnumberformatsymbols_ext.js
@@ -27446,6 +27446,7 @@ goog.i18n.CompactNumberFormatSymbols_zh_Hant_TW =
 
 /**
  * Selected compact number formatting symbols by locale.
+ * @private
  */
 goog.i18n.CompactNumberFormatSymbols = goog.i18n.CompactNumberFormatSymbols_en;
 
